f4c48514e4ad140e83f4639a64ae1980c1e4e0f5
KanaKarimata <kana.lt22@gmail.com>
2022-08-27 18:07:43 +0900

    finish part9-106

--- script.js
+++ script.js
@@ -42,8 +42,15 @@ const restaurant = {
 
   orderPasta: function(ing1, ing2, ing3) {
     console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}`);
+  },
+
+  orderPizza: function(mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
   }
 };
+
+// 1) Destructuring
 
 //Rest Pattern and Parameter
 //SPREAD because on RIGHT side of =
@@ -58,6 +65,29 @@ const [pizza, , risotto, ...otherFood] = [
   ...restaurant.starterMenu
 ];
 console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays} = restaurant.openingHours
+console.log(weekdays);
+
+
+// 2) Functions
+const add = function(...numbers) {
+  let sum = 0;
+  for(let i = 0; i<numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+
+restaurant.orderPizza('mushrooms');
 
 //Spread Operator
 // const arr = [7, 8, 9];
